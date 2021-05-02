890e78f543e1a3e55b8c70ec8d8d983f808060d7
michaelb <michaelpb@gmail.com>
2021-05-02 10:47:56 -0700

    Work on hacky ssg

--- src/modulocli-utils.js
+++ src/modulocli-utils.js
@@ -1,7 +1,17 @@
+const fs = require('fs');
+const pathlib = require('path');
+const {JSDOM} = require('jsdom');
+const {DOMParser} = require('xmldom');
 
 function exitErr(message) {
     console.warn(message);
     process.exit(1);
+}
+
+function assert(value, ...info) {
+    if (!value) {
+        exitErr(`ERROR: ${Array.from(info).join(' ')}`);
+    }
 }
 
 function parseArgs(argArray) {
@@ -18,6 +28,12 @@ function parseArgs(argArray) {
             }
             if (arg.startsWith('--')) {
                 currentFlag = arg.slice(2);
+
+                if (arg.includes('=')) {
+                    const [name, value] = arg.split('=');
+                    currentFlag = null;
+                    args.flags[name] = value;
+                }
             } else {
                 currentFlag = null;
                 for (const singleChar of arg.split('')) {
@@ -26,6 +42,7 @@ function parseArgs(argArray) {
             }
         } else if (currentFlag) {
             args.flags[currentFlag] = arg;
+            currentFlag = null;
         } else if (args.command === null) {
             args.command = arg;
         } else {
@@ -35,11 +52,169 @@ function parseArgs(argArray) {
     return args;
 }
 
-function checkArgs(split, commands) {
+function checkArgs(args, commands) {
     // presently no-op
+    const options = Object.keys(commands).join(' or ');
+    const cmd = Array.from(process.argv).join(' ');
+    if (args.command) {
+        if (!(args.command in commands)) {
+            exitErr(`Unknown: '${args.command}' (Expected: ${options})`);
+        }
+    } else {
+        exitErr(`Usage: ${cmd} [${options}]`);
+    }
+}
+
+function loadModuloDocument(path) {
+    /*
+      Very hacky function to load Modulo and mock set it up with a path to
+      a given HTML file. The JSDOM document is returned.
+    */
+    return setupModulo(path);
+
+    // Very simple hacky way to do mocked web-components define
+    function webComponentsUpgrade(dom, el, cls, secondTime=false) {
+        // Manually "upgrading" the JSDOM element with the webcomponent
+        const instance = new cls();
+        const protos = [instance, Reflect.getPrototypeOf(instance)];
+        if (!el.tagName.startsWith('MOD-')) {
+            protos.push(Reflect.getPrototypeOf(protos[1]));
+        }
+        protos.reverse();
+        const allKeys = [];
+        for (const proto of protos) {
+            allKeys.push(...Reflect.ownKeys(proto));
+        }
+        // console.log(el.tagName, 'this is all props', allKeys);
+        for (const key of allKeys) {
+            if (instance[key] instanceof Function) {
+                el[key] = instance[key].bind(el);
+            } else {
+                el[key] = instance[key];
+            }
+        }
+        if (el.connectedCallback && !secondTime) {
+            // console.log('connected callback for:', el.tagName);
+            //setTimeout(() => {
+            //}, 0);
+            el.connectedCallback();
+        }
+    }
+
+    function clearRequireCache(searchPath) {
+        const path = pathlib.resolve(__dirname, searchPath);
+        delete require.cache[path];
+        // Just clear anyhting with Modulo
+        for (const key of Object.keys(require.cache)) {
+            if (key.includes('Modulo')) {
+                // console.log('this is key', key);
+                delete require.cache[key];
+            }
+        }
+        //require.cache = {};
+    }
+
+    function setupModulo(path = null, includeDebugger = false, html = '') {
+        let Modulo;
+        if (includeDebugger) {
+            Modulo = require('./ModuloDebugger.js');
+        } else {
+            Modulo = require('./Modulo.js');
+        }
+        const htmlCode = path ? fs.readFileSync(path, 'utf-8') : html;
+        const dom = new JSDOM(htmlCode);
+        Modulo.document = dom.window.document; // for easier testing
+        Modulo.globals.DOMParser = DOMParser;
+        Modulo.globals.HTMLElement.prototype.getAttribute = a => 'XYZYX getAttribute plcholder hack';
+        Modulo.globals.HTMLElement.prototype.hasChildNodes = a => false; // HACK
+        Modulo.globals.window =  dom.window;
+        Modulo.globals.document =  dom.window.document;
+        Modulo.globals.mockConsole = [];
+        const {mockConsole} = Modulo.globals;
+        Modulo.globals.console = {log: mockConsole.push.bind(mockConsole)};
+        Modulo.globals.DocumentFragment =  dom.window.DocumentFragment;
+        Modulo.globals.mockRegistered = [];
+        Modulo.globals.mockMounted = [];
+        Modulo.globals.mockTimeouts = [];
+        const {mockTimeouts} = Modulo.globals;
+        const mockTOPush = (func, time) => mockTimeouts.push({func, time});
+        Modulo.globals.setTimeout = mockTOPush;
+        Modulo.globals.setInterval = mockTOPush;
+
+        Modulo.globals.mockModifyFile = [];
+
+        Modulo.globals.MutationObserver = class {
+            observe(el) {
+                const {setAttribute} = el;
+                el.setAttribute = (...args) => {
+                    //console.log('fake set attribute happening!', args);
+                    setAttribute.apply(el, args);
+                    el.attributeMutated();
+                };
+            }
+        };
+
+        Modulo.globals.fetch = url => {
+            // Faked version of fetch
+            const rootDir = pathlib.dirname(path);
+            const fullPath = pathlib.join(rootDir, url);
+            const response = {
+                text: () => {},
+                // later, can add "json" that just sets a var
+            };
+            return {
+                then: callback => {
+                    callback(response);
+                    return {
+                        then: callback => {
+                            let data = fs.readFileSync(fullPath, 'utf-8');
+                            for (const func of Modulo.globals.mockModifyFile) {
+                                data = func(fullPath, data);
+                            }
+                            callback(data);
+                        }
+                    };
+                },
+            }
+        };
+
+        Modulo.globals.customElements = {
+            define: (name, cls) => {
+                const elements = dom.window.document.querySelectorAll(name);
+                for (const el of elements) {
+                    webComponentsUpgrade(dom, el, cls);
+                    Modulo.globals.mockMounted.push({el, cls});
+                    //setTimeout(() => {
+                    //}, 0);
+                }
+                Modulo.globals.mockRegistered.push({name, cls});
+            },
+        };
+        if (path) {
+            Modulo.defineAll();
+        }
+        return Modulo;
+    }
+}
+
+function walkSync(basePath) {
+    const results = [];
+    const bareFileNames = fs.readdirSync(basePath);
+    for (const baseName of bareFileNames) {
+        file = basePath + '/' + baseName;
+        const stat = fs.statSync(file);
+        if (stat && stat.isDirectory()) {
+            results = results.concat(walkSync(file));
+        } else {
+            results.push(file);
+        }
+    }
+    return results;
 }
 
 module.exports = {
+  assert,
   checkArgs,
   parseArgs,
+  walkSync,
 }
--- src/modulocli.js
+++ src/modulocli.js
@@ -1,11 +1,32 @@
 //const Modulo = require('./src/Modulo');
-const utils = require('./modulossg-utils');
+const utils = require('./modulocli-utils');
+
+const ssgHandlers = {
+    html: (input, output, filenames) => {
+    },
+    default: (input, output, filenames) => {
+    },
+}
 
 const commands = {
+    ssg: args => {
+        console.log('this is args', args);
+        const inputDir = args.positional[0];
+        const outputDir = args.flags.output;
+        utils.assert(inputDir, 'Specify a source directory');
+        utils.assert(outputDir, 'Specify output dir, e.g. --output=docs/');
+        const filenames = utils.walkSync(inputDir);
+        for (const inputPath of filenames) {
+            const outputPath = ouputDir + inputPath.slice(inputDir.length);
+            console.log(outputPath);
+        }
+        //utils.loadModuloDocument(
+    },
     buildjs: args => {
+        // BROKEN
         const loaders = [];
-        assert(args.namespace, 'Must specify namespace')
-        assert(args.src, 'Must specify src')
+        utils.assert(args.namespace, 'Must specify namespace')
+        utils.assert(args.src, 'Must specify src')
         fs.readFile(filename, 'utf8', (err, data) => {
             if (err) throw err;
             const loader = new Modulo.Loader(args.namespace, args.src);
@@ -19,7 +40,7 @@ const commands = {
 function main(argv) {
     const args = utils.parseArgs(Array.from(argv));
     utils.checkArgs(args, commands);
-    //commands[args.command](args);
+    commands[args.command](args);
 }
 
 main(process.argv);
