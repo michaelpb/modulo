a240e22ffeac29824a32355b633714fef64b3639
michaelb <michaelpb@gmail.com>
2021-09-28 21:37:43 -0700

    adding third party JS

---
+++ docs-src/js/thirdparty/codemirror_5.63.0/build.sh
@@ -0,0 +1,24 @@
+# Order in example:
+# <link rel="stylesheet" href="../../lib/codemirror.css">
+# <link rel="stylesheet" href="../../theme/mdn-like.css">
+# <script src="../../lib/codemirror.js"></script>
+# <script src="../../addon/mode/overlay.js"></script>
+# <script src="../xml/xml.js"></script>
+# <script src="../htmlmixed/htmlmixed.js"></script>
+# <script src="django.js"></script>
+
+cat\
+    lib/codemirror.js\
+    addon/mode/overlay.js\
+    mode/xml/xml.js\
+    mode/css/css.js\
+    mode/javascript/javascript.js\
+    mode/htmlmixed/htmlmixed.js\
+    mode/django/django.js\
+    > codemirror_bundled.js
+
+cat\
+    lib/codemirror.css\
+    theme/eclipse.css\
+    > codemirror_bundled.css
+
